1815a1d923462d6fe546cbed58cf9077e65a8458
taylor-musso <taylormusso04@gmail.com>
2026-03-24 19:24:58 -0700

    made csv to json converter

---
+++ csv_to_json.py
@@ -0,0 +1,18 @@
+data_path = "data/Webpages_Classification_test_data.csv"
+
+import csv
+import json
+
+csv_file = data_path
+json_file = "data/webpages_classification.json"
+
+with open(csv_file, mode="r", encoding="utf-8") as infile, \
+     open(json_file, mode="w", encoding="utf-8") as outfile:
+
+    reader = csv.DictReader(infile)
+
+    for row in reader:
+        json_line = json.dumps(row)
+        outfile.write(json_line + "\n")
+
+print("Done! NDJSON file created.")
